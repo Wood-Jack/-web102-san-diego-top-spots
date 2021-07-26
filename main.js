98830e57e66c48101d9b0cb9012727e4124b027b
Wood-Jack <thewoodrowz@gmail.com>
2021-07-26 14:22:54 -0400

    Finished loading data in table headers need to be fixed to be at top of page

--- main.js
+++ main.js
@@ -1,4 +1,31 @@
-$(document).ready(function() {
+
+$(document).ready(function(){
 // write your code here
+
+
+
+//downloading the contents of the json file
+
+    $.getJSON('data.json', function(data){
+       
+    // creating for loop to loop through creation of the table.
+
+        for(var i = 0; i < data.length ; i++){
+
+            var table = document.getElementById('table');
+            var row = table.insertRow(i);
+
+            var names = row.insertCell(0);
+            var description = row.insertCell(1);
+            var location = row.insertCell(2);
+
+            names.innerText = data[i].name;
+            description.innerText = data[i].description;
+            location.innerText = ("https://www.google.com/maps?q=33.09745,-116.99572" + data[i].location + "'>'" + 'Link' + "</a>");
+
+
+        }
+
+    })
 
 });
